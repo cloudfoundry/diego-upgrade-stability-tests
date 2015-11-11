a8146bf56f8c52c7fec096826f36af29006ffe68
Jen Spinney <jen.spinney@hpe.com>
2015-11-11 15:48:10 -0800

    Testing cloning specific versions of repos

--- diego_upgrade_stability_test.go
+++ diego_upgrade_stability_test.go
@@ -34,11 +34,21 @@ var _ = Describe("Upgrade Stability Tests", func() {
 		Expect(sess).NotTo(Say("cf-warden-diego-database"))
 
 		By("Generating the V0 deployment manifests for 5 piece wise deployments")
+		cloneCfCommand := exec.Command("bash", "-c", "'mkdir -p repos && cd repos && git clone -b v220 https://github.com/cloudfoundry/cf-release && cd cf-release && git submodule update --init src/loggregator'")
+		sess, err = Start(cloneCfCommand, GinkgoWriter, GinkgoWriter)
+		Expect(err).NotTo(HaveOccurred())
+
+		cloneDiegoCommand := exec.Command("bash", "-c", "'cd repos && git clone -b v0.1434.0 https://github.com/cloudfoundry-incubator/diego-release'")
+		sess, err = Start(cloneDiegoCommand, GinkgoWriter, GinkgoWriter)
+		Expect(err).NotTo(HaveOccurred())
+
 		generateManifestCmd := exec.Command("./scripts/generate-manifests",
-			"-d", config.V0DiegoReleasePath,
-			"-c", config.V0CfReleasePath,
+			"-d", "repos/diego-release",
+			"-c", "repos/cf-release",
 			"-l",
 		)
+		sess, err = Start(cloneCfCommand, GinkgoWriter, GinkgoWriter)
+		Expect(err).NotTo(HaveOccurred())
 		sess, err = Start(generateManifestCmd, GinkgoWriter, GinkgoWriter)
 		Expect(err).NotTo(HaveOccurred())
 		Eventually(sess, COMMAND_TIMEOUT).Should(Exit(0))
