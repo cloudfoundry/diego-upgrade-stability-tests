c89c6ed3a39b348879a9328e0c5761ba4af5b833
John Shahid <jvshahid@gmail.com>
2017-03-13 09:42:44 -0700

    add port 4443 to the list of forwarded ports
    [#134315447]
    
    Signed-off-by: Nima Kaviani <nkavian@us.ibm.com>

--- scripts/add_ha_proxy_resource_pool_to_cf_manifest.rb
+++ scripts/add_ha_proxy_resource_pool_to_cf_manifest.rb
@@ -6,7 +6,7 @@ cf_manifest = YAML.load_file ARGV[0]
 
 cf_manifest['resource_pools'] << {
   "name" => "ha_proxy_z1",
-  "cloud_properties" => {"name" => "random", "ports" => [{"host" => 80},{"host" => 443},{"host" => 2222}]},
+  "cloud_properties" => {"name" => "random", "ports" => [{"host" => 80},{"host" => 443},{"host" => 2222},{"host" => 4443}]},
   "stemcell" => cf_manifest["resource_pools"][0]["stemcell"],
   "network" => cf_manifest["resource_pools"][0]["network"],
   "env" => cf_manifest["resource_pools"][0]["env"],
